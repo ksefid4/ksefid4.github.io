f46e4d49cf23f2352464662a537c87d832fd20b2
ksefid4 <ksefid4@gmail.com>
2025-07-27 10:25:24 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,10 +2,10 @@ export default {
   async fetch(request) {
     const url = new URL(request.url);
     if (url.pathname === "/sub") {
-      return new Response("پاسخ مسیر /sub با موفقیت ارسال شد ✅", {
+      return new Response("✅ مسیر /sub با موفقیت هندل شد", {
         headers: { "Content-Type": "text/plain" },
       });
     }
-    return new Response("مسیر یافت نشد ❌", { status: 404 });
+    return new Response("❌ مسیر یافت نشد", { status: 404 });
   },
 };
